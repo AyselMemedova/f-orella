0fd3f2e193dbb930218bab1c69981916fcf0d0c8
AyselMemedova <memmdovaysel@gmail.com>
2024-04-28 21:44:04 +0400

    first commit

---
+++ fiorella.js
@@ -0,0 +1,103 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var mySwiper = new Swiper('.swiper-container', {
+//       direction: 'horizontal', // or 'vertical'
+//       loop: true,
+//       centeredSlides: true,
+
+//       // If we need pagination
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         dynamicBullets: true,
+//       },
+
+//       // Navigation arrows
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+
+//       // Autoplay
+//       autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//       },
+
+//       // Use slide or fade transition effect
+//       speed: 2000,
+//       effect: 'fade',
+//       fadeEffect: {
+//             crossFade: true,
+//        },
+//     });
+//   });
+
+// init Isotope
+let $grid = $(".grid").isotope({
+  // options
+});
+// filter items on button click
+$(".filter-button-group").on("click", "li", function () {
+  let filterValue = $(this).attr("data-filter");
+  $grid.isotope({ filter: filterValue });
+});
+
+const ikinciFilter = document.querySelector(".ikinci_filter");
+let flowers;
+const basketItemsList = document.querySelector("#basketItemsList");
+const basketItems = [];
+
+if (localStorage.getItem("flowers")) {
+  flowers = JSON.parse(localStorage.getItem("flowers"));
+} else {
+  flowers = [];
+  localStorage.setItem("flowers", JSON.stringify(flowers));
+}
+
+fetch("./api.json")
+  .then((resp) => resp.json())
+  .then((data) => {
+    renderUI(data);
+  });
+
+function renderUI(item) {
+  ikinciFilter.innerHTML = "";
+  for (let i = 0; i < item.length; i++) {
+    ikinciFilter.innerHTML += `
+        <div class="col-lg-3 * ${item[i].category}">
+            <img src="${item[i].img}" alt="">
+            <span>${item[i].name}</span> <br>
+            <div>            
+            <span> $${item[i].price}</span>
+            <button onclick="addToBasket(${item[i].id}) "><i class="fa-solid fa-bag-shopping"  ></i></button>
+            </div>
+        </div>
+        `;
+  }
+}
+
+function renderBasket(basketItems) {
+  for (let i = 0; i < basketItems.length; i++) {
+    basketItemsList +=`
+      <tr>
+      <th scope="row"><img src="${basketItems[i].img}" alt="" width="50px" height="60px"></th>
+      <td>${basketItems[i].name}</td>
+      <td>$259</td>
+      <td><button style="border: none; color: #f34f3f;"><i class="fa-regular fa-trash-can"></i> </button></td>
+    </tr>
+      `;
+  }
+}
+console.log(flowers);
+
+function addToBasket(id) {
+  fetch("./api.json")
+    .then((resp) => resp.json())
+    .then((data) => {
+        const target = data.find((flowers) => flowers.id == id);
+        flowers.push(target);
+        renderBasket(basketItems);
+        console.log(flowers);
+    });
+  
+}
